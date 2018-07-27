a440c2033ce7cf03207be250b19cfa11b9c20256
FuzzyBuckBeak <FuzzyBuckBeak@gmail.com>
2018-07-27 13:18:29 -0700

    Changes

--- LeetCode/LeetCode/Transpose Matrix.swift
+++ LeetCode/LeetCode/Transpose Matrix.swift
@@ -6,6 +6,16 @@
 //  Copyright © 2018 FuzzyBuckBeak. All rights reserved.
 //
 
+
+/*************************************************************************************************************************************************************
+Given a matrix A, return the transpose of A.
+ 
+The transpose of a matrix is the matrix flipped over it's main diagonal, switching the row and column indices of the matrix.
+*************************************************************************************************************************************************************/
+
+//Input: [[1,2,3],[4,5,6],[7,8,9]]
+//Output: [[1,4,7],[2,5,8],[3,6,9]]
+
 import Foundation
 
 class Transpose {
