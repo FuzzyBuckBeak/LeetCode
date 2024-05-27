7e2f994bebde718ce5ca818fc54aeb44408dbf14
Apoorva Shettigar <shettigarapoorva@gmail.com>
2024-05-26 22:01:44 -0700

    Update 20. Valid Parentheses.swift

--- LeetCode/LeetCode/20. Valid Parentheses.swift
+++ LeetCode/LeetCode/20. Valid Parentheses.swift
@@ -60,3 +60,34 @@ class ValidParentheses {
         return isValid
     }
 }
+
+
+
+//using Array:
+
+
+func isValid(_ s: String) -> Bool {
+   var stack: [Character] = []
+   for char in s {
+       if char == "(" || char == "[" || char == "{" {
+           stack.append(char)
+       } else {
+               switch char {
+               case ")": let value = stack.last
+                   if value == "(" { stack.removeLast() } else { stack.append(char)}
+                   
+                   
+               case "]": let value = stack.last
+                   if value == "[" { stack.removeLast() } else { stack.append(char)}
+                   
+               case "}": let value = stack.last
+                   if value == "{" { stack.removeLast() } else { stack.append(char)}
+                   
+                   
+               default: break
+               }
+           
+       }
+   }
+   return stack.count == 0
+}
